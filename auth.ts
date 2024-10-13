3345ca8fc26660878861d7e225302fda3f2e3d38
MAHB98 <m.alihooshmand@gmail.com>
2024-10-13 15:38:15 +0330

    try 3 for commit 11

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
 import { db } from "./lib/database";
+import { DefaultSession } from "next-auth";
 declare module "@auth/core/adapters" {
   interface AdapterUser {
     password: string;
@@ -13,27 +14,39 @@ declare module "next-auth" {
   interface User {
     role?: string;
   }
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      address: string;
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession["user"];
+  }
 }
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google,
     Github,
     credentials({
-      // async authorize(credentials) {
-      //   console.log(credentials.email);
+      authorize: async (credentials) => {
+        console.log(credentials.email);
 
-      //   if (!credentials.email || !credentials.password) return null;
-      //   const getUser = await db.getUserByEmail!(credentials.email as string);
-      //   if (!getUser) return null;
-      //   console.log(getUser);
+        if (!credentials.email || !credentials.password) return null;
+        const getUser = await db.getUserByEmail!(credentials.email as string);
+        if (!getUser) return null;
+        console.log(getUser);
 
-      //   const compare = await bcrypt.compare(
-      //     credentials.password as string,
-      //     getUser.password
-      //   );
-      //   if (!compare) return null;
-      //   return getUser;
-      // },
+        // const compare = await bcrypt.compare(
+        //   credentials.password as string,
+        //   getUser.password
+        // );
+        // if (!compare) return null;
+        return null;
+      },
       credentials: {
         email: {
           label: "email",
